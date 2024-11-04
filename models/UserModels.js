73a7cdcd9ff42fb91bcacdd81ec78641606bc0eb
Yssrrr20 <muhyassar20@gmail.com>
2024-11-04 14:40:58 +0700

    Update UserModels.js

--- models/UserModels.js
+++ models/UserModels.js
@@ -17,14 +17,31 @@ const user = db.define("user", {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true
+        validate: {
+            isEmail: true  // Pastikan email foratnya valdi
+        }
     },
     password : {
         type: DataTypes.STRING,
         allowNull: false
+         validate: {
+            len: [6, 100]  // Passwordnya minimal 6 karakter
+        }
     }
 },{
     freezeTableName:true
 });
+
+// Hook untuk melakukan hash password sebelum menyimpan ke database
+user.beforeCreate(async (user) => {
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+});
+
+// Instance method untuk verifikasi password
+user.prototype.verifyPassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
 
 export default user;
 
